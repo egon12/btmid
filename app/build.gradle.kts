5814b812f92ad9de696fa926391b7e58aabe908b
egon12 <egon.firman@gmail.com>
2026-06-28 15:04:44 +0700

    failed to use 1.0.1 try to use 1.0.2 for release in bitrise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "org.gilbertxenodike.btmid"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
