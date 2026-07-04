55f72366e52544c80f97f66077a0b88f4981c9ca
egon12 <egon.firman@gmail.com>
2026-07-04 23:23:24 +0700

    bump version 1.0.3+3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "org.gilbertxenodike.btmid"
         minSdk = 36
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
